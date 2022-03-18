6904c78abc5a73024e6fa8587dad70494bc3420d
ksg0605 <ksg0605@gmail.com>
2022-03-18 18:10:42 +0900

    add UserDaoTests

---
+++ userdao2/src/test/java/kr/ac/jejunu/UserDaoTests.java
@@ -0,0 +1,32 @@
+package kr.ac.jejunu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * packageName            : kr.ac.jejunu
+ * fileName              : UserDaoTests
+ * author                : sunkyu
+ * date                  : 2022/03/18
+ * description           :
+ * ===========================================================
+ * DATE              AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 2022/03/18           sunkyu             최초 생성
+ */
+public class UserDaoTests {
+    @Test
+    public void findById() {
+        Integer id = 1;
+        String name = "Hulk";
+        String password = "1234";
+
+        UserDao userDao = new UserDao();
+        User user = userDao.findById(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+}
