5f8715afc0eaec2d50f12b4d50cf0fee8ff04a2a
ksg0605 <ksg0605@gmail.com>
2022-03-18 18:10:57 +0900

    add UserDao

---
+++ userdao2/src/main/java/kr/ac/jejunu/UserDao.java
@@ -0,0 +1,19 @@
+package kr.ac.jejunu;
+
+/**
+ * packageName            : kr.ac.jejunu
+ * fileName              : UserDao
+ * author                : sunkyu
+ * date                  : 2022/03/18
+ * description           :
+ * ===========================================================
+ * DATE              AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 2022/03/18           sunkyu             최초 생성
+ */
+public class UserDao {
+    public User findById(Integer id) {
+
+        return null;
+    }
+}
