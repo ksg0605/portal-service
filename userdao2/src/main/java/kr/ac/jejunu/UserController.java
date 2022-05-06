942473b63ef95c3b2b07e528a169d0daf0f4261c
ksg0605 <ksg0605@gmail.com>
2022-05-06 17:53:55 +0900

    spring mvc 2

---
+++ userdao2/src/main/java/kr/ac/jejunu/UserController.java
@@ -0,0 +1,33 @@
+package kr.ac.jejunu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * packageName            : kr.ac.jejunu
+ * fileName              : UserController
+ * author                : sunkyu
+ * date                  : 2022/05/06
+ * description           :
+ * ===========================================================
+ * DATE              AUTHOR              NOTE
+ * -----------------------------------------------------------
+ * 2022/05/06           sunkyu             최초 생성
+ */
+//@RequestMapping("/user")
+@Controller
+public class UserController {
+    @RequestMapping("/user2")
+    public ModelAndView user(@RequestParam("id") Integer id, @RequestParam("name") String name) {
+
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        ModelAndView modelAndView = new ModelAndView("user");
+        modelAndView.addObject(user);
+        return modelAndView;
+
+    }
+}
