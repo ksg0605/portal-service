01356daf146d8c070f573a919e0596b92f63c04d
ksg0605 <ksg0605@gmail.com>
2022-06-07 15:52:41 +0900

    multipart resolve

--- userdao2/src/main/java/kr/ac/jejunu/UserController.java
+++ userdao2/src/main/java/kr/ac/jejunu/UserController.java
@@ -2,8 +2,16 @@ package kr.ac.jejunu;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * packageName            : kr.ac.jejunu
@@ -19,6 +27,25 @@ import org.springframework.web.servlet.ModelAndView;
 //@RequestMapping("/user")
 @Controller
 public class UserController {
+
+    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+    public void upload() {
+
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public ModelAndView upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        File path = new File(request.getServletContext().getRealPath("/") + "/WEB-INF/static/" + file.getOriginalFilename());
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        bufferedOutputStream.write(file.getBytes());
+        bufferedOutputStream.close();
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("url", "/images/" + file.getOriginalFilename());
+        return modelAndView;
+    }
+
     @RequestMapping("/user2")
     public ModelAndView user(
             @RequestParam("id") Integer id,
